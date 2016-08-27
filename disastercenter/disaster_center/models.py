4efb03ff4d1640fd40277797ab17f1a35c46306f
Benson Polican <bensonpolican@yahoo.com>
2016-08-27 15:47:21 +0800

    fixed models

--- disastercenter/disaster_center/models.py
+++ disastercenter/disaster_center/models.py
@@ -4,20 +4,23 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):  
     user = models.OneToOneField(User)  
-    exp = models.IntegerField(blank=True,null=True)
+    email = models.CharField(max_length=30,default="default@gmail.com")  
+    first_name = models.CharField(max_length=30,default="default")  
+    last_name = models.CharField(max_length=30,default="default")  
+    exp = models.IntegerField(default=0)
 
 
 
     def __str__(self):  
           return "%s's profile" % self.user 
 
-class Reports(models.Model):
+class Report(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	reportType = models.CharField(max_length=30)
 	latitude = models.FloatField()
 	longitude = models.FloatField()
 	dateTimeStart = models.DateField().auto_now
-	dateTimeEnd = models.DateField()
+	dateTimeEnd = models.DateField(default="00/00/00").auto_now
 
 
 
